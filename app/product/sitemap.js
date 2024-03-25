94c177c43548ff0c47219556159f9dc0633a05c8
pradyumnsuper <pradyumnsuper@gmail.com>
2024-03-25 10:35:06 +0530

    fsa

--- app/product/sitemap.js
+++ app/product/sitemap.js
@@ -4,7 +4,7 @@ import products from '@/lib/products'; // Import the products array
 
 export async function generateSitemaps() {
   // Calculate the number of sitemaps needed based on product count and limit
-  const numSitemaps = Math.ceil(products.length / 1000); // Google's limit is 50,000 URLs per sitemap
+  const numSitemaps = Math.ceil(products.length / 5000); // Google's limit is 50,000 URLs per sitemap
   const sitemaps = [];
   for (let i = 0; i < numSitemaps; i++) {
     sitemaps.push({ id: i });
@@ -14,8 +14,8 @@ export async function generateSitemaps() {
 
 export default async function sitemap({ id }) {
   // Calculate start and end indexes for products in this sitemap
-  const start = id * 1000;
-  const end = Math.min(start + 1000, products.length); // Ensure end doesn't exceed product count
+  const start = id * 5000;
+  const end = Math.min(start + 5000, products.length); // Ensure end doesn't exceed product count
 
   // Slice the products array to get products for this sitemap
   const retrievedProducts = products.slice(start, end);
