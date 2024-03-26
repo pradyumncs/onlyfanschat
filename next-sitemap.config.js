3ee21940d512e79655d98abadd5b88c9172acffa
pradyumnsuper <pradyumnsuper@gmail.com>
2024-03-26 17:10:57 +0530

    fd

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,7 @@ module.exports = {
     // Add the three URLs to the sites array
     robotsTxtOptions: {
     additionalSitemaps:[
+      'https://onlyfansai.online/products/sitemap/0.xml',
       'https://onlyfansai.online/products/sitemap/1.xml' ,
       'https://onlyfansai.online/products/sitemap/2.xml' ,
       'https://onlyfansai.online/products/sitemap/3.xml' ,
