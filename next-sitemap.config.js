d9fa7b8b65267b23a24220c64f07100235a3fe17
pradyumnsuper <pradyumnsuper@gmail.com>
2024-03-26 16:57:35 +0530

    fds

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    siteUrl: "https://onlyfansai.online",
+    generateRobotsTxt: true,
+    // Add the three URLs to the sites array
+    robotsTxtOptions: {
+    additionalSitemaps:[
+      'https://onlyfansai.online/products/sitemap/1.xml' ,
+      'https://onlyfansai.online/products/sitemap/2.xml' ,
+      'https://onlyfansai.online/products/sitemap/3.xml' ,
+      'https://onlyfansai.online/products/sitemap/4.xml'
+    ],
+    }
+  };
+  
